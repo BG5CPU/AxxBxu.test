d93ee3c700c79d4ca321c48416c2e6409b6bcc03
Lidong <bg5cpu@gmail.com>
2025-09-26 20:06:27 +0800

    physical_mpc_sin_a to vK

--- SimD/data_pre_process_07.m
+++ SimD/data_pre_process_07.m
@@ -4,10 +4,10 @@ clear; clc; close all;
 
 % file path
 file1 = 'Gazebo_data_csv/mpc_sin_0926A/imu_data.csv';
-file2 = 'Gazebo_data_csv/0.4vk_0.1sin_80/attitude_r_p_y.csv';
-file3 = 'Gazebo_data_csv/0.4vk_0.1sin_80/actuator_control.csv';
-file4 = 'Gazebo_data_csv/0.4vk_0.1sin_80/input_x_y_z.csv';
-file5 = 'Gazebo_data_csv/0.4vk_0.1sin_80/pose.csv';
+file2 = 'Gazebo_data_csv/mpc_sin_0926A/attitude_r_p_y.csv';
+file3 = 'Gazebo_data_csv/mpc_sin_0926A/actuator_control.csv';
+file4 = 'Gazebo_data_csv/mpc_sin_0926A/input_x_y_z.csv';
+file5 = 'Gazebo_data_csv/mpc_sin_0926A/pose.csv';
 
 
 % read (imu_data.csv)
@@ -57,82 +57,99 @@ check_data_continuity(matrix_pose(:,2), 'matrix_pose');
 
 % time difference
 
-matrix_actuator_control = matrix_actuator_control(2:end,:);
-
 figure(1); hold on;
-plot(matrix_input_x_y_z(:,1), 'r.');
+plot(diff(matrix_input_x_y_z(:,1)), 'r.');
+plot(diff(matrix_input_x_y_z(:,3)), 'g.');
+hold off;
+
+[~, idx_matrix_input_x_y_z_1] = unique(matrix_input_x_y_z(:,1), 'stable');
+[~, idx_matrix_input_x_y_z_3] = unique(matrix_input_x_y_z(:,3), 'stable');
+
+
+figure(2); hold on;
+plot(diff(matrix_actuator_control(:,1)), 'r.');
+plot(diff(matrix_actuator_control(:,3)), 'g.');
+hold off;
+
+[~, idx_matrix_actuator_control_1] = unique(matrix_actuator_control(:,1), 'stable');
+[~, idx_matrix_actuator_control_3] = unique(matrix_actuator_control(:,3), 'stable');
+matrix_actuator_control = matrix_actuator_control(idx_matrix_actuator_control_3, :);
+
+
+matrix_input_x_y_z = matrix_input_x_y_z(2:end,:);
+
+figure(3); hold on;
+plot(matrix_actuator_control(:,3), 'r.');
 plot(matrix_input_x_y_z(:,3), 'g.');
 hold off;
 
-figure(2); 
-plot(matrix_actuator_control(:,1)-matrix_input_x_y_z(:,1), 'r.');
+figure(4)
+plot(matrix_actuator_control(:,6)-matrix_input_x_y_z(:,6),'r.');
 
 
-
-% [~, idx] = unique(matrix_actuator_control(:,3), 'stable');   % indices of first occurrence
-% matrix_actuator_control = matrix_actuator_control(idx, :);
-
-figure(2); hold on;
-plot(diff(matrix_input_x_y_z(:,1)), 'b.');
-plot(diff(matrix_input_x_y_z(:,3)), 'k*');
-hold off;
-
-figure(3); hold on;
-plot(matrix_actuator_control(:,1), 'r.');
-plot(matrix_actuator_control(:,3), 'g.');
-plot(matrix_input_x_y_z(:,1), 'b.');
-plot(matrix_input_x_y_z(:,3), 'k.');
-hold off;
-
-
-
-figure(4);
-plot(matrix_actuator_control(:,4) - matrix_input_x_y_z(:,4));
-
+matrix_imu_data = matrix_imu_data(29:end,:);
 figure(5); hold on;
-plot(matrix_input_x_y_z(:,1)-matrix_attitude_r_p_y(:,1), 'r.');
-plot(matrix_input_x_y_z(:,3)-matrix_attitude_r_p_y(:,3), 'g.');
-hold off;
-
-
-matrix_imu_data = matrix_imu_data(40:end,:);
-figure(6); hold on;
 plot(flipud(matrix_input_x_y_z(:,3)), 'r.'); % flipud
 plot(flipud(matrix_imu_data(:,3)), 'g.');
 hold off;
 
 
-pose_angle_x = matrix_attitude_r_p_y(:,[1,4]);
-pose_angle_y = matrix_attitude_r_p_y(:,[1,5]);
-pose_angle_z = matrix_attitude_r_p_y(:,[1,6]);
+matrix_attitude_r_p_y = matrix_attitude_r_p_y(2:end,:);
+figure(6); hold on;
+plot((matrix_input_x_y_z(:,3)), 'r.');
+plot((matrix_attitude_r_p_y(:,3)), 'g.');
+hold off;
 
-velocity_angle_x = interpolate_to_match_A(matrix_input_x_y_z(:,3), matrix_imu_data(:,[1,8]));
-velocity_angle_y = interpolate_to_match_A(matrix_input_x_y_z(:,3), matrix_imu_data(:,[1,9]));
-velocity_angle_z = interpolate_to_match_A(matrix_input_x_y_z(:,3), matrix_imu_data(:,[1,10]));
+Tlen_matrix_input_x_y_z = -matrix_input_x_y_z(1,3)+matrix_input_x_y_z(end,3);
+Nlen_matrix_input_x_y_z = round(Tlen_matrix_input_x_y_z/2e6);
+time = matrix_input_x_y_z(1,3) + (0:Nlen_matrix_input_x_y_z-1)*2e6;
+time = time';
+
+
+pose_angle_x = interpolate_to_match_A(time, matrix_attitude_r_p_y(:,[3,4]));
+pose_angle_y = interpolate_to_match_A(time, matrix_attitude_r_p_y(:,[3,5]));
+pose_angle_z = interpolate_to_match_A(time, matrix_attitude_r_p_y(:,[3,6]));
+
+velocity_angle_x = interpolate_to_match_A(time, matrix_imu_data(:,[3,8]));
+velocity_angle_y = interpolate_to_match_A(time, matrix_imu_data(:,[3,9]));
+velocity_angle_z = interpolate_to_match_A(time, matrix_imu_data(:,[3,10]));
 
 
 figure(7); hold on;
+plot(pose_angle_x(:,2), 'r');
+plot(pose_angle_y(:,2), 'g');
+plot(pose_angle_z(:,2), 'b');
+hold off;
+
+figure(8); hold on;
 plot(velocity_angle_x(:,2), 'r');
 plot(velocity_angle_y(:,2), 'g');
 plot(velocity_angle_z(:,2), 'b');
 hold off;
 
 
+actuator_control_x = interpolate_to_match_A(time, matrix_input_x_y_z(:,[3,4]));
+actuator_control_y = interpolate_to_match_A(time, matrix_input_x_y_z(:,[3,5]));
+actuator_control_z = interpolate_to_match_A(time, matrix_input_x_y_z(:,[3,6]));
 
-actuator_control_x = matrix_input_x_y_z(:,[1,4]);
-actuator_control_y = matrix_input_x_y_z(:,[1,5]);
-actuator_control_z = matrix_input_x_y_z(:,[1,6]);
+
+figure(9); hold on;
+plot(actuator_control_x(:,2), 'r');
+plot(actuator_control_y(:,2), 'g');
+plot(actuator_control_z(:,2), 'b');
+hold off;
 
 
-data_input_output_06 = [actuator_control_x, actuator_control_y, actuator_control_z, ...
+
+data_input_output_07 = [actuator_control_x, actuator_control_y, actuator_control_z, ...
                         pose_angle_x, pose_angle_y, pose_angle_z, ...
                         velocity_angle_x, velocity_angle_y, velocity_angle_z];
 
 
-Pstart01 = 4530;
-Pend01 = 9230;
-% data_input_output_06 = data_input_output_06(Pstart01:Pend01 , :);
-% save('./Gazebo_data_mat/data06.mat', 'data_input_output_06');
+Pstart01 = 4400;
+Pend01 = 26400;
+data_input_output_07 = data_input_output_07(Pstart01:Pend01 , :);
+save('./Gazebo_data_mat/data07.mat', 'data_input_output_07');
 
 
 
